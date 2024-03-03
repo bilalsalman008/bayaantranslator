8f73e219472ea1ad4e571102f3b065c11b4a76a2
bilalsalman008 <161752598+bilalsalman008@users.noreply.github.com>
2024-03-02 22:27:42 -0800

    Update node.js

--- node.js
+++ node.js
@@ -5,11 +5,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Bayaan Translator!');
+});
+
 app.post('/translate', async (req, res) => {
   try {
-    // Dynamic import for ESM compatibility
     const fetch = await import('node-fetch');
-    const apiKey = 'AIzaSyAq5GlJNnQaA253zywityNt73bV7YZ1TBk';
+    const apiKey = 'YOUR_GOOGLE_TRANSLATE_API_KEY';
     const apiUrl = 'https://translation.googleapis.com/language/translate/v2';
 
     const response = await fetch.default(`${apiUrl}?key=${apiKey}`, {
